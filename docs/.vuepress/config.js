0bdc3e1b80ed84ff9d7da451271f160de4a67943
yx <15876302883@163.com>
2019-09-19 16:57:52 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,91 @@
+module.exports = {
+  title: '网站标题',
+  description: '网站描述',
+  // 注入到当前页面的 HTML <head> 中的标签
+  port:80,//端口
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+  ],
+  base: '/', // 这是部署到github相关的配置 下面会讲
+  markdown: {
+    lineNumbers: true // 代码块显示行号
+  },
+  themeConfig: {
+    sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
+    lastUpdated: 'Last Updated' ,// 文档更新时间：每个文件git最后提交的时间
+	nav:[// 添加导航栏
+      { text: '前端算法', link: '/algorithm/' }, // 内部链接 以docs为根目录
+      { text: '博客', link: 'http://obkoro1.com/' }, // 外部链接
+      // 下拉列表
+      {
+        text: 'GitHub',
+        items: [
+          { text: 'GitHub地址', link: 'https://github.com/OBKoro1' },
+          {
+            text: '算法仓库',
+            link: 'https://github.com/OBKoro1/Brush_algorithm'
+          }
+        ]
+      }        
+    ],
+    // 为以下路由添加侧边栏
+    sidebar:{
+      '/guide/': [
+        {
+          title: '布局类组件',
+          collapsable: true,
+          children: [
+            'test/test1',
+            'test/test2',
+            'test/test3',
+            'test/test4',
+          ]
+        },
+        {
+          title: '可视化组件',
+          collapsable: true,
+          children: [
+          ]
+        },
+        {
+          title: '工具类组件',
+          collapsable: true,
+          children: [
+          ]
+        },
+        {
+          title: '方法类函数',
+          collapsable: true,
+          children: [
+          ]
+        }
+      ],
+      '/knowledge/': [
+        {
+          title: 'CSS知识库',
+          collapsable: false,
+          children: [
+          ]
+        },
+        {
+          title: 'JS知识库',
+          collapsable: false,
+          children: [
+          ]
+        },
+        {
+          title: 'node知识库',
+          collapsable: false,
+          children: [
+          ]
+        },
+        {
+          title: 'vue知识库',
+          collapsable: false,
+          children: [
+          ]
+        }
+      ]
+    }
+  }
+};
